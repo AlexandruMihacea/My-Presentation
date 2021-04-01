25f059e3ef0ca6a23d840f0fb75b53da79344be3
Beta97 <alexmihacea@gmail.com>
2021-04-01 17:34:32 +0300

    photo folder done

--- src/SecondCard.js
+++ src/SecondCard.js
@@ -9,9 +9,9 @@ export default function ImgMediaCard() {
 
   return (
       <div className="Photo">
-      <img className="PhotoOne" src={require('./firstPhoto.jpg').default} height={300} ></img>
+      <img className="PhotoOne" src={require('./Photo/firstPhoto.jpg').default} height={300} ></img>
       <h1 className="text">I play the guitar </h1>
-      <img className="PhotoTwo" src={require('./secondPhoto.png').default} height={300} ></img>
+      <img className="PhotoTwo" src={require('./Photo/secondPhoto.png').default} height={300} ></img>
       <h1 className="text">I love snowboarding !</h1>
     </div>
   );
